d9e63eb06814fc3acf86192902f7fc921d41a770
Basscord <brian.mau@brian-mau.com>
2017-03-29 12:46:58 -0700

    Wired up MCController

--- Helm/MCTestViewController.swift
+++ Helm/MCTestViewController.swift
@@ -12,17 +12,19 @@ class MCTestViewController: UIViewController, UITextFieldDelegate {
 
 
     override var prefersStatusBarHidden: Bool { return true}
-    
+    var myMCController = MCController.sharedInstance
 
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        }
+        self.myMCController.setup()
+    }
     
     
     @IBAction func sendCommand(_ sender: UIButton) {
         let command = sender.titleLabel!.text
         print("command: \(command!)")
+        self.myMCController.sendCommand(text: command!)
         
     }
     
